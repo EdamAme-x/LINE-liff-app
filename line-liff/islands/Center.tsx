d2ef254691dd5a68ed5827a76f97b636a605e2aa
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-07-07 21:12:53 +0900

    fix

--- line-liff/islands/Center.tsx
+++ line-liff/islands/Center.tsx
@@ -1,5 +1,8 @@
 import { Head, IS_BROWSER } from "$fresh/runtime.ts";
+//import liff from "@sky-liff";
 import { useRef, useEffect } from "preact/hooks";
+
+console.log(IS_BROWSER)
 
 export default function Tool() {
   let inputRef = useRef<HTMLInputElement>(null);
@@ -7,56 +10,56 @@ export default function Tool() {
 
   useEffect(() => {
     if (IS_BROWSER) {
-      import("@sky-liff").then((liff) => {
-        liff
-          .init({
-            liffId: liffId,
-          })
-          .then(() => {
-            alert("起動しました。");
-          })
-          .catch((err) => {
-            alert(err);
-          });
-      });
+      liff
+        .init({
+          liffId: liffId,
+        })
+        .then(() => {
+          alert("起動しました。");
+        })
+        .catch((err) => {
+          alert(err);
+        });
     }
   }, []);
 
   function SendMessage() {
     if (inputRef.current) {
-      try {
-        import("@sky-liff").then((liff) => {
-          liff.sendMessages([{ type: "text", text: inputRef.current.value }]);
-          alert("送信しました。");
-          inputRef.current.value = "ok";
-        });
-      }catch (err) {
-        alert(err);
-      }
+      liff.sendMessages([{ type: "text", text: inputRef.current.value }]);
+      alert("送信しました。");
+      inputRef.current.value = "ok";
     }
   }
 
-  return (
-    <>
-      <Head>
-        <title>LINE-Tool-Kit</title>
-        <meta name="description" content="LINE-Tool-Kit by @macl2189 / amex" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div>
-        <h1>LINE-Tool-Kit</h1>
-        <p>Test: SendMessage</p>
-        <input
-          type="text"
-          class="border"
-          placeholder="Message"
-          ref={inputRef}
-        />
-        <button class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors">
-          Send!
-        </button>
-        <p>LINE-Tool-Kit by @macl2189 / amex </p>
-      </div>
-    </>
-  );
+  // ブラウザ上の場合のみコンポーネントを表示する
+  if (IS_BROWSER) {
+    return (
+      <>
+        <Head>
+          <title>LINE-Tool-Kit</title>
+          <meta
+            name="description"
+            content="LINE-Tool-Kit by @macl2189 / amex"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div>
+          <h1>LINE-Tool-Kit</h1>
+          <p>Test: SendMessage</p>
+          <input
+            type="text"
+            class="border"
+            placeholder="Message"
+            ref={inputRef}
+          />
+          <button class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors">
+            Send!
+          </button>
+          <p>LINE-Tool-Kit by @macl2189 / amex </p>
+        </div>
+      </>
+    );
+  } else {
+    return <div>Error</div>;
+  }
 }
